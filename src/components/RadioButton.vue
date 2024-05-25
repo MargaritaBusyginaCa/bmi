7716c71c1e697b4b16e91c7c969261d97ab60989
00008758 <busygina.rita.ca@gmail.com>
2024-05-24 22:14:08 -0400

    add a custom radio button

--- src/components/RadioButton.vue
+++ src/components/RadioButton.vue
@@ -4,34 +4,35 @@ import { ref } from "vue";
 const picked = ref("metric");
 const emit = defineEmits(["select"]);
 
-function select() {
+function select(btnIndex) {
+  picked.value = btnIndex === 0 ? "metric" : "imperial";
   emit("select", picked.value);
+}
+function test(btnIndex) {
+  const selectedBtn = document.getElementsByClassName("radio-btn");
+  for (let i = 0; i < selectedBtn.length; i++) {
+    if (selectedBtn[i].classList.contains("selected")) {
+      selectedBtn[i].classList.remove("selected");
+    }
+  }
+  selectedBtn[btnIndex].classList.add("selected");
 }
 </script>
 
 <template>
   <div class="mb-5 grid grid-cols-2 md:flex md:gap-[50px] lg:grid lg:gap-0">
-    <div>
-      <input
-        type="radio"
-        id="metric"
-        value="metric"
-        v-model="picked"
-        class="radio checked:bg-indigo-500"
-        @change="select"
-      />
-      <label for="metric">Metric</label>
+    <div class="flex gap-3 items-center">
+      <div class="w-[31px] h-[31px] flex justify-center items-center">
+        <div class="radio-btn" @click="test(0), select(0)"></div>
+      </div>
+      <span for="imperial">Metric</span>
     </div>
-    <div>
-      <input
-        type="radio"
-        id="imperial"
-        value="imperial"
-        v-model="picked"
-        class="radio checked:bg-indigo-500"
-        @change="select"
-      />
-      <label for="imperial">Imperial</label>
+    <div class="flex gap-3 items-center">
+      <div class="w-[31px] h-[31px] flex justify-center items-center">
+        <div class="radio-btn" @click="test(1), select(1)"></div>
+      </div>
+
+      <span for="imperial">Imperial</span>
     </div>
   </div>
 </template>
@@ -39,6 +40,21 @@ function select() {
 <style scoped lang="scss">
 input[type="radio"] {
   margin-right: 15px;
-  vertical-align: top;
+  vertical-align: center;
+  transform: scale(2);
+}
+.radio-btn {
+  width: 31px;
+  height: 31px;
+  border-radius: 50%;
+  border: 1px solid grey;
+  background-color: white;
+  &.selected {
+    width: 20px;
+    height: 20px;
+    background-color: #345ff6;
+
+    box-shadow: 0px 0px 0px 10px #3662ff4d;
+  }
 }
 </style>
